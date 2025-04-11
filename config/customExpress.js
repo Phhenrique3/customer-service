295b67d555cd6c75579b85ff5af45fdaa6076113
Pedro Henrique <127359574+Phhenrique3@users.noreply.github.com>
2025-04-11 13:43:50 -0300

    info

--- config/customExpress.js
+++ config/customExpress.js
@@ -2,16 +2,17 @@
 const express = require("express");
 
 const consign = require("consign"); // Importa o módulo "consign", que é usado para carregar automaticamente arquivos e módulos em uma aplicação.
+const conexao = require("../infraestrutura/conexao.js").default;
 
-
-module.exports = () => {   // Exporta uma função que configura e retorna uma instância do aplicativo Express.
-  const app = express()   // Cria uma instância do aplicativo Express, que será usada para configurar e gerenciar o servidor.
-  app.use(express.urlencoded({extended: true})) // Configura o middleware para processar dados enviados em formulários (application/x-www-form-urlencoded).
+module.exports = () => {
+  // Exporta uma função que configura e retorna uma instância do aplicativo Express.
+  const app = express(); // Cria uma instância do aplicativo Express, que será usada para configurar e gerenciar o servidor.
+  app.use(express.urlencoded({ extended: true })); // Configura o middleware para processar dados enviados em formulários (application/x-www-form-urlencoded).
   // A opção "extended: true" permite que objetos aninhados sejam analisados.
-  app.use(express.json()) // Configura o middleware para processar dados enviados no formato JSON (application/json).
+  app.use(express.json()); // Configura o middleware para processar dados enviados no formato JSON (application/json).
   // Isso é útil para APIs que recebem dados no corpo das requisições.
   consign() // Usa o módulo "consign" para carregar automaticamente os arquivos da pasta "controllers".// Esses arquivos são injetados na instância do aplicativo Express, permitindo que os controladores sejam acessados.
-      .include('controllers')
-      .into(app)
-  return app
-}
+    .include("controllers")
+    .into(app);
+  return app;
+};
--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 const customExpress = require("./config/customExpress.js");
+const conexao = require("./infraestrutura/conexao.js");
+
+conexao.connect((erro) => {
+  if (erro) {
+    console.error("Erro ao conectar ao banco de dados:", erro);
+  } else {
+    console.log("Conectado com sucesso ao banco de dados!");
+  }
+});
 
 const app = customExpress();
 
