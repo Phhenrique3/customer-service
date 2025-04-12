639aeee85c383530347722c7d51c529bf7fd1479
Pedro Henrique <127359574+Phhenrique3@users.noreply.github.com>
2025-04-12 16:48:18 -0300

    From Atendimento

--- controllers/atendimento.js
+++ controllers/atendimento.js
@@ -5,13 +5,15 @@ const Atendimento = require("../models/atendimentos");
 module.exports = (app) => {
   app.get("/atendimentos", (req, res) => {
     res.send("Bem-vindo no atendimento !");
+    
   });
 
   app.post("/atendimentos", (req, res) => {
     const atendimento = req.body;
 
     Atendimento.adiciona(atendimento)
-    res.send("agendado com sucesso ");
-  });
+    res.send(` Atendimento ${atendimento.cliente} adicionado com sucesso!`);
+    console.log(atendimento);
+    });
 };
 
