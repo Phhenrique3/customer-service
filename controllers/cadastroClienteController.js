542654f30935bbfba846725fd1d9326ddbde3217
Pedro Henrique <127359574+Phhenrique3@users.noreply.github.com>
2025-11-17 00:32:03 -0300

    model cadastro add cpf + table bd

--- controllers/cadastroClienteController.js
+++ controllers/cadastroClienteController.js
@@ -1,30 +1,43 @@
-
-const CadastroCliente = require('../models/cadastroClienteModel');
-const bcrypt = require('bcrypt');
+const CadastroCliente = require("../models/cadastroClienteModel");
+const bcrypt = require("bcrypt");
 
 const cadastra = async (req, res) => {
-    const cliente = req.body;
-    console.log('Dados recebidos:', cliente);
+  const cliente = req.body;
+  console.log("Dados recebidos:", cliente);
 
-    if (!cliente.nome || !cliente.email || !cliente.telefone || !cliente.senha) {
-      return res.status(400).json({ erro: 'Todos os campos são obrigatórios' });
+  if (
+    !cliente.nome ||
+    !cliente.email ||
+    !cliente.telefone ||
+    !cliente.senha ||
+    !cliente.cpf
+  ) {
+    return res.status(400).json({ erro: "Todos os campos são obrigatórios" });
+  }
+
+  try {
+    const clienteExistente = await CadastroCliente .buscaPorCPF(cliente.cpf);
+
+    if (clienteExistente) {
+      return res
+        .status(409)
+        .json({ erro: "Já existe um usuário cadastrado com esse CPF/CNPJ" });
     }
 
-    try {
-      // Hash da senha
-      const hashSenha = await bcrypt.hash(cliente.senha, 10);
-      cliente.senha = hashSenha; // substitui senha
+    // Hash da senha
+    cliente.senha = await bcrypt.hash(cliente.senha, 10);
 
-      // Chama método para inserir no banco e espera o retorno com o usuário criado
-      const usuario = await CadastroCliente.adiciona(cliente);
+    // Inserindo no banco
+    const clienteCriado = await CadastroCliente.adiciona(cliente);
 
-      // Retorna o sucesso com dados do usuário criado
-      return res.status(200).json({mensagem: 'Cadastro realizado com sucesso'});
-    } catch (error) {
-      console.error('Erro ao cadastrar cliente:', error);
-      return res.status(500).json({ erro: 'Erro interno do servidor' });
-    }
-  };
+    return res
+      .status(201)
+      .json({ mensagem: "Cadastro realizado com sucesso", cliente: clienteCriado });
 
+  } catch (error) {
+    console.error("Erro ao cadastrar cliente:", error);
+    return res.status(500).json({ erro: "Erro interno do servidor" });
+  }
+};
 
-module.exports = { cadastra }
+module.exports = { cadastra };
--- models/cadastroClienteModel.js
+++ models/cadastroClienteModel.js
@@ -1,16 +1,22 @@
-const conexao = require('../infraestrutura/conexao.js')
+const conexao = require("../infraestrutura/conexao.js");
 
 class CadastroCliente {
   adiciona(cadastroCliente, res) {
-    const sql = 'INSERT INTO Clientes SET ?'
+    const sql = "INSERT INTO Clientes SET ?";
     conexao.query(sql, cadastroCliente, (erro, resultado) => {
       if (erro) {
-        res.status(400).json({ erro: erro.message })
+        res.status(400).json({ erro: erro.message });
       } else {
-        res.status(201).json({ id: resultado.insertId, ...cadastroCliente })
+        res.status(201).json({ id: resultado.insertId, ...cadastroCliente });
       }
-    })
+    });
+  }
+
+  async buscaPorCPF(cpf) {
+    const sql = "SELECT * FROM Clientes WHERE cpf = ? LIMIT 1";
+    const [linhas] = await conexao.query(sql, [cpf]);
+    return linhas[0];
   }
 }
 
-module.exports = new CadastroCliente()
+module.exports = new CadastroCliente();
